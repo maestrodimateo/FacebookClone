e55107921dfa4361ef61b4d965e334349bb5abbb
Noel MEBALE <noel.mebale@aninf.ga>
2022-06-10 17:17:53 +0100

    best update

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -10,83 +10,92 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  static const double _icons_size = 20;
-  static const Color facebookBlue = Color(0xFF395A9A);
-  static const Color facebookGrey = Color(0xFF9A9A9A);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
         elevation: 0,
+        backgroundColor: Colors.transparent,
         leading: Icon(
           Icons.camera_alt,
-          color: Colors.black87,
+          color: Colors.black,
         ),
         title: SizedBox(
           height: 40,
           child: TextField(
             decoration: InputDecoration(
-              prefixIcon: Icon(Icons.search),
-              labelStyle:
-                  TextStyle(color: facebookGrey, fontWeight: FontWeight.bold),
-              labelText: 'Search',
-              fillColor: Color(0xFFF2F2F6),
-              filled: true,
-              enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                      color: Color.fromARGB(255, 224, 225, 228), width: 0.5),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(50),
-                  )),
-              focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.green),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(50),
-                  )),
-            ),
+                contentPadding: EdgeInsets.symmetric(vertical: 0),
+                hintText: 'Search',
+                hintStyle:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.grey),
+                labelStyle: TextStyle(color: Colors.black),
+                filled: true,
+                fillColor: Colors.grey.shade100,
+                prefixIcon: Icon(Icons.search),
+                focusColor: Colors.grey,
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.grey.shade400, width: 0.5),
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.grey.shade400, width: 0.5),
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                )),
           ),
         ),
         actions: [
           Padding(
-            padding: const EdgeInsets.only(right: 15),
+            padding: const EdgeInsets.only(right: 20),
             child: Image(
-              width: 40,
               image: AssetImage('assets/icons/messenger.png'),
-              semanticLabel: 'messenger logo',
+              width: 30,
             ),
           )
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 18, vertical: 25),
+
+      //body
+      body: SafeArea(
+          child: SingleChildScrollView(
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Stories',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                ),
-                Row(
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
-                      'See active',
-                      style: TextStyle(),
+                      'Stories',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.grey.shade700),
                     ),
-                    Icon(
-                      LucideIcons.chevronRight,
-                      size: _icons_size,
+                    Row(
+                      children: [
+                        Text(
+                          'See Archive',
+                          style: TextStyle(
+                              fontSize: 15, color: Colors.grey.shade700),
+                        ),
+                        Icon(
+                          LucideIcons.chevronRight,
+                          size: 16,
+                          color: Colors.grey,
+                        )
+                      ],
                     )
                   ],
                 ),
-              ],
+              ),
             ),
-            SizedBox(height: 15),
+            //Stories
             Container(
-              height: 140,
+              padding: EdgeInsets.only(left: 20),
+              height: 150,
               child: ListView(
                 scrollDirection: Axis.horizontal,
                 children: [
@@ -96,213 +105,106 @@ class _HomePageState extends State<HomePage> {
                   Story(),
                   Story(),
                   Story(),
+                  Story(),
                 ],
               ),
             ),
-            SizedBox(height: 25),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 12,
-                          backgroundImage:
-                              AssetImage('assets/images/profil.jpg'),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  CircleAvatar(
-                                    backgroundColor: Colors.white,
-                                    child: Icon(
-                                      Icons.people,
-                                      color: Colors.black,
-                                      size: 8,
-                                    ),
-                                    radius: 5,
-                                  ),
-                                ],
+
+            //update info
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 25, horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Stack(
+                        children: [
+                          CircleAvatar(
+                              radius: 15,
+                              backgroundImage:
+                                  AssetImage('assets/images/profil.jpg')),
+                          Positioned(
+                            bottom: 0,
+                            left: 18,
+                            child: CircleAvatar(
+                              backgroundColor: Colors.white,
+                              radius: 7,
+                              child: Icon(
+                                Icons.people,
+                                size: 10,
+                                color: Colors.black,
                               ),
-                            ],
+                            ),
                           ),
-                        ),
-                        SizedBox(width: 10),
-                        Column(
+                        ],
+                      ),
+                      SizedBox(width: 10),
+                      Container(
+                        height: 40,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text('Jordan Praise',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            RichText(
+                                text: TextSpan(
+                                    style: TextStyle(color: Colors.black),
+                                    children: [
+                                  TextSpan(
+                                    text: 'Jordan Praise',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15),
+                                  ),
+                                  TextSpan(text: '  updated his cover photo'),
+                                ])),
+                            SizedBox(
+                              height: 2,
+                            ),
+                            Text(
+                              '33 mins ago',
+                              style: TextStyle(
+                                  fontSize: 11, color: Colors.grey.shade600),
+                            )
                           ],
                         ),
-                        SizedBox(width: 3),
-                        Text('updated his cover photo')
-                      ],
-                    )
-                  ],
-                ),
-                Icon(Icons.more_horiz)
-              ],
-            ),
-            SizedBox(height: 25),
-            Container(
-              height: 200,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.black38,
-                      blurRadius: 30,
-                      spreadRadius: -80,
-                      offset: Offset(0, 100)),
+                      )
+                    ],
+                  ),
+                  Icon(
+                    Icons.more_horiz,
+                    color: Colors.grey,
+                  )
                 ],
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage('assets/images/mountains.jpg')),
               ),
             ),
-            SizedBox(height: 25),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Stack(
-                  alignment: Alignment.center,
-                  clipBehavior: Clip.none,
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: Colors.blue,
-                      child: Icon(
-                        Icons.thumb_up,
-                        color: Colors.white,
-                        size: 10,
-                      ),
-                      radius: 10,
-                    ),
-                    Positioned(
-                      left: 15,
-                      child: CircleAvatar(
-                        radius: 11.5,
-                        backgroundColor: Colors.white,
-                        child: CircleAvatar(
-                          backgroundColor: Color(0xFFF44234),
-                          child: Icon(
-                            Icons.favorite,
-                            color: Colors.white,
-                            size: 10,
-                          ),
-                          radius: 10,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      left: 45,
-                      child: Text(
-                        '400',
-                        style: TextStyle(
-                            fontSize: 12, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ],
-                ),
-                Text(
-                  '122 Comments',
-                  style: TextStyle(fontSize: 12),
-                )
-              ],
-            ),
-            Divider(),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Row(
-                  children: [
-                    Icon(
-                      Icons.thumb_up_alt_outlined,
-                      size: 15,
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      'Like',
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.messenger_outline,
-                      size: 15,
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      'Comment',
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                )
-              ],
+            Container(
+              height: 200,
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage('assets/images/mountains.jpg'), fit: BoxFit.cover),
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  color: Colors.amber),
             )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(
-          LucideIcons.plus,
-          size: 35,
-        ),
-        backgroundColor: facebookBlue,
-        onPressed: () => {},
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.only(top: 30),
-        child: BottomNavigationBar(
-          selectedItemColor: facebookBlue,
-          unselectedItemColor: facebookGrey,
-          iconSize: _icons_size,
-          backgroundColor: Colors.transparent,
-          elevation: 0,
+      )),
+
+      bottomNavigationBar: BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
+          elevation: 0,
           items: [
-            BottomNavigationBarItem(
-              icon: Icon(LucideIcons.layout),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  LucideIcons.users,
-                ),
-                label: ''),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  LucideIcons.youtube,
-                ),
-                label: ''),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  LucideIcons.bell,
-                ),
-                label: ''),
+            BottomNavigationBarItem(icon: Icon(LucideIcons.layout), label: ''),
+            BottomNavigationBarItem(icon: Icon(LucideIcons.users), label: ''),
+            BottomNavigationBarItem(icon: Icon(LucideIcons.youtube), label: ''),
+            BottomNavigationBarItem(icon: Icon(LucideIcons.bell), label: ''),
             BottomNavigationBarItem(
                 icon: CircleAvatar(
                   backgroundImage: AssetImage('assets/images/profil.jpg'),
                   radius: 15,
                 ),
-                label: ''),
-          ],
-        ),
-      ),
+                label: '')
+          ]),
     );
   }
 }
